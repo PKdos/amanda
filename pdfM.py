858d79f9d4a98cf1774e6aa26a7feea1228d8d90
PKdos <prenkisn@gmail.com>
2017-03-23 02:34:49 +0100

    Update and rename pdfM02.py to pdfM.py

--- pdfM.py
+++ pdfM.py
@@ -2,9 +2,8 @@
 
 from PyPDF2 import PdfFileReader, PdfFileMerger
 
-obje = '/tmp/amanda/proba06/pdf/p_especialidad_mir_hh.pdf'
 def pdfM(dir_arch):
-#    try:
+    try:
         merger = PdfFileMerger()
         pdfF = merger(PdfFileReader(dir_arch, "rb"))
         dicI = pdfF.getDocumentInfo()
@@ -14,6 +13,5 @@ def pdfM(dir_arch):
         for metaItem in dicI:
             text.write("[+] " + metaItem.encode('utf-8') + ":" + dicI[metaItem].encode('utf-8') + '\n')
         text.close()
-#    except:
-#        print "Fallo al abrir pdf"
-pdfM(obje)
+    except:
+        print "Fallo al abrir pdf"
