8ee79781650ce1a26019a0eafda73f76d5d21452
PKdos <prenkisn@gmail.com>
2017-03-23 03:12:44 +0100

    Update pilm.py

--- pilm.py
+++ pilm.py
@@ -3,8 +3,6 @@
 from PIL.ExifTags import TAGS
 from PIL import Image
 
-obje = '/tmp/amanda/proba2/imagenes/amhh-white.png'
-
 def pilM(x):
     try:
         imgF = Image.open(x)
@@ -12,4 +10,3 @@ def pilM(x):
         print info
     except:
         print "archivo no encontrado"
-pilM(obje)
